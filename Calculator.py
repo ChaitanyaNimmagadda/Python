a36588fcdb0a355a5814b3cfcc8c3f5b604889c3
ChaitanyaNimmagadda <89669784+ChaitanyaNimmagadda@users.noreply.github.com>
2021-08-29 22:20:18 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,7 @@
+print("enter first number")
+n1 = input()
+print("enter second number")
+n2 = input()
+print("enter third number")
+n3 = input()
+print("sum of these numbers",int(n1) + int(n2) * int(n3))
---
+++ Dictionary.py
@@ -0,0 +1,6 @@
+Dc1 = {"Water":"used to Drink"
+       ,"Hot water":"Used to Bath"
+       ,"Cold water":"party"}
+print("Ask_a_word")
+Ask_a_word=input()
+print(Dc1[Ask_a_word])
